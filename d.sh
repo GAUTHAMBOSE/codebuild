6694537bbca0f7915234e09f1e5acfe352c1ca5c
GAUTHAMBOSE <gauthambosec.com>
2023-09-07 12:11:12 +0530

    vi.4

--- d.sh
+++ d.sh
@@ -46,19 +46,5 @@ done
 # done
 
 
-# #!/bin/bash
-
-# for i in kk bose gg; do
-#     if [[ "$i" == *"kk"* ]]; then
-#         echo "kk.txt file changes"
-#         # aws sns publish --topic-arn arn:aws:sns:us-east-1:824738494196:temp --message "kkk.txt folder changes"
-#         echo "SNS notification sent to email"
-#     fi
-#     if [[ "$i" == *"bose"* ]]; then
-#         echo "bose.txt has changed"
-#         #aws sns publish --topic-arn arn:aws:sns:us-east-1:824738494196:temp --message "bose.txt has changed"
-#         echo "SNS notification sent to email"
-#     fi
-# done
 
 
